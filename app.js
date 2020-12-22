20939e084d62cd7ddbb44b9b3f955a607d258646
Camila Cortes <camilacortes@Camilas-MacBook-Air.local>
2020-12-22 11:16:28 -0700

    updated the style. Fixed the image upload issue.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const form = document.form;
 const button = document.getElementById('button');
 
@@ -19,11 +17,33 @@ function getData(){
 
 function addingToList(data){
   document.getElementById('display-box').innerHTML = ""
+
   for(let i = 0; i < data.length; i++){
-    const h1 = document.createElement('h1')
-    h1.innerHTML = data[i].title;
-    h1.className = 'listed-items'
-    document.getElementById('display-box').appendChild(h1) 
+
+    const htitle = document.createElement('h1')
+    const hprice = document.createElement('h2');
+    const hdescription = document.createElement('h2');
+    const image = document.createElement('img');
+    //title
+    htitle.textContent = `Title: ${data[i].title}`;
+    htitle.className = 'title'
+    //price
+    hprice.textContent = `Price: ${data[i].price}`;
+    hprice.className = 'h2';
+    // description
+    hdescription.textContent = `Description: ${data[i].description}`
+    hdescription.className = 'h2';
+    // image 
+    image.src = data[i].imgUrl;
+    image.class= "image";
+    // image.className = 'h2';
+    
+    // appending to the DOM 
+    document.getElementById('display-box').appendChild(htitle) 
+    document.getElementById('display-box').appendChild(hprice);
+    document.getElementById('display-box').appendChild(hdescription);
+    document.getElementById('display-box').appendChild(image);
+
   }
 }
 
@@ -37,7 +57,7 @@ form.addEventListener('submit', (e) => {
   title: document.form.title.value,
   price : document.form.price.value,
   description : document.form.description.value,
-  image : document.form.image.value
+  imgUrl : document.form.image.value
   }
 
  axios.post('https://api.vschool.io/Camila/todo/', newToDo)
